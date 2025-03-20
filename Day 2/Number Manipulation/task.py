0f6073ed3bb5692100667a810d0efb38c5766d60
Palak Dhanadia <98673846+Palak-Dhanadia@users.noreply.github.com>
2025-03-20 15:43:26 +0530

    Add files via upload

---
+++ Day 2/Number Manipulation/task.py
@@ -0,0 +1,19 @@
+bmi = 84 / 1.65 ** 2
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi))
+
+print(round(bmi, 2))
+
+score = 0
+height = 1.8
+is_winning = True
+
+# User score a point
+score += 1
+print(score)
+
+# f-strings
+print(f"Your score is ={score}")
---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,10 @@
+"""print(type("Hello"))
+print(type(123))
+print(type(123.45))
+print(type(True))"""
+
+# print(int("123") + int("456"))
+
+username = input("Enter your name")
+length = len(username)
+print("Number of letters in your name: " + str(length))
